41db2cb96260fd76cda4765b4268380592387e76
Maria Ronauli <mronauli@gmail.com>
2020-02-02 20:28:04 -0700

    add tests for show pet and shelter pets index

--- spec/features/shelters/shelter_pets/visitor_can_see_all_pets_at_a_shelter_spec.rb
+++ spec/features/shelters/shelter_pets/visitor_can_see_all_pets_at_a_shelter_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "pets' index page", type: :feature do
-  context "as a visitor" do
-    it "can see each pet and its' picture, name, approximate age, sex and name of shelter it's located" do
+RSpec.describe "on the shelter pets index" do
+  describe "as a visitor" do
+    it "sees pets and all its information including the shelter id" do
       shelter_1 = Shelter.create(name: "Dumb Friends League",
                                  address: "3",
                                  city: "Denver",
@@ -22,13 +22,15 @@ RSpec.describe "pets' index page", type: :feature do
                                 sex: "female",
                                 shelter: shelter_1,
                                 adoption_status: "adoptable")
+
       pet_2 = Pet.create(picture: "/assets/porkie.jpg",
                                 name: "Angus",
                                 description: "Not into yoga",
                                 age: "1",
                                 sex: "male",
-                                shelter: shelter_2,
+                                shelter: shelter_1,
                                 adoption_status: "adoptable")
+
       pet_3 = Pet.create(picture: "/assets/tyga.jpg",
                                 name: "Tyga",
                                 description: "Has half a brain",
@@ -37,17 +39,22 @@ RSpec.describe "pets' index page", type: :feature do
                                 shelter: shelter_2,
                                 adoption_status: "not adoptable")
 
-      visit "/pets"
-      expect(page).to have_css("img[src*='/assets/dog-landing-hero-lg.jpg']")
-      expect(page).to have_content(pet_1.name)
-      expect(page).to have_content(pet_1.age)
-      expect(page).to have_content(pet_1.sex)
-      expect(page).to have_content(pet_1.shelter.name)
-      expect(page).to have_css("img[src*='/assets/porkie.jpg']")
-      expect(page).to have_content(pet_2.name)
-      expect(page).to have_content(pet_2.age)
-      expect(page).to have_content(pet_2.sex)
-      expect(page).to have_content(pet_2.shelter.name)
+    visit "/shelters/#{shelter_1.id}/pets"
+
+    expect(page).to have_css("img[src*='dog-landing-hero-lg.jpg']")
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.age)
+    expect(page).to have_content(pet_1.shelter.name)
+
+    expect(page).to have_css("img[src*='porkie.jpg']")
+    expect(page).to have_content(pet_2.name)
+    expect(page).to have_content(pet_2.age)
+    expect(page).to have_content(pet_2.shelter.name)
+
+    expect(page).to_not have_css("img[src*='tyga.jpg']")
+    expect(page).to_not have_content(pet_3.name)
+    expect(page).to_not have_content(pet_3.age)
+    expect(page).to_not have_content(pet_3.shelter.name)
     end
   end
 end
