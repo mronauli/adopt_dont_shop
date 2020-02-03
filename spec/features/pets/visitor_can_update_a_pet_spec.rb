36bf05e63df1c9be4dba180a4afa6d8fa659a5e4
Maria Ronauli <mronauli@gmail.com>
2020-02-02 20:54:29 -0700

    add remaining usability tests

--- spec/features/pets/visitor_can_update_a_pet_spec.rb
+++ spec/features/pets/visitor_can_update_a_pet_spec.rb
@@ -1,54 +1,53 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "on the shelter pets index", type: :feature do
+RSpec.describe "on a pet's show page", type: :feature do
   describe "as a visitor" do
-    it "can click a link that takes them to a page where they can add a pet for adoption" do
-      shelter_1 = Shelter.create(name: "Dumb Friends League", address: "2080 S Quebec St", city: "Denver", state: "CO", zip: "81029")
-
-      visit "/shelters/#{shelter_1.id}/pets"
-      expect(page).to have_content(shelter_1.name)
-
-      click_link "Create Pet"
-      expect(current_path).to eq("/shelters/#{shelter_1.id}/pets/new")
-    end
-    it "can fill out a form to add a pet for adoption" do
+    it "can click a link that takes them to a page where they can edit a pet's information" do
       shelter_1 = Shelter.create(name: "Dumb Friends League",
-                                 address: "2080 S Quebec St",
+                                 address: "3",
                                  city: "Denver",
                                  state: "CO",
                                  zip: "81029")
 
-      pet_1 = Pet.create(picture: "/assets/porkie.jpg",
-                                name: "Angus",
-                                description: "Not into yoga",
-                                age: "1",
-                                sex: "male",
-                                shelter: shelter_1,
-                                adoption_status: "adoptable")
+      pet_1 = Pet.create(picture: "/assets/dog-landing-hero-lg.jpg",
+                        name: "Foxy",
+                        description: "Enjoys pina coladas and getting caught in the rain",
+                        age: "2",
+                        sex: "female",
+                        shelter: shelter_1,
+                        adoption_status: "adoptable")
 
-      picture = "app/assets/images/pitbull.jpg"
-      name = "Bruno"
-      description = "Likes getting caught in the rain"
-      age = "3"
-      sex = "female"
-      adoption_status = "adoptable"
+      visit "/pets/#{pet_1.id}"
+      expect(page).to have_content(pet_1.name)
 
-      visit "/shelters/#{shelter_1.id}/pets/new"
-      attach_file(:picture, picture)
-      fill_in :name, with: name
-      fill_in :description, with: description
-      fill_in :age, with: age
+      click_link "Update Pet"
+      expect(current_path).to eq("/pets/#{pet_1.id}/edit")
+    end
+    it "can fill out a form and update a particular pet" do
+      shelter_1 = Shelter.create(name: "Dumb Friends League",
+                                 address: "3",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "81029")
+
+      pet_1 = Pet.create(picture: "/assets/dog-landing-hero-lg.jpg",
+                        name: "Foxy",
+                        description: "Enjoys pina coladas and getting caught in the rain",
+                        age: "2",
+                        sex: "female",
+                        shelter: shelter_1,
+                        adoption_status: "adoptable")
+
+      visit "/pets/#{pet_1.id}"
+      click_link "Update Pet"
+      expect(current_path).to eq("/pets/#{pet_1.id}/edit")
+
+      adoption_status = "not adoptable"
+
       fill_in :adoption_status, with: adoption_status
-      select :female, from: :sex
-
-      click_on "Create Pet"
-      expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
-      # expect { click_on "Create Pet" }.to change(Pet, :count).by(1)
-      expect(page).to have_css("img[src*='/assets/pitbull-a5c2c1ca32533e109a762f24fa5e3d14009344f48816dccee4c5a55c0871931f.jpg']")
-      expect(page).to have_content(name)
-      expect(page).to have_content(description)
-      expect(page).to have_content(age)
-      expect(page).to have_content(sex)
+      click_button "Submit"
+      expect(current_path).to eq("/pets/#{pet_1.id}")
+      expect(page).to have_content("not adoptable")
     end
   end
 end
--- spec/features/shelters/visitor_can_see_all_shelters_spec.rb
+++ spec/features/shelters/visitor_can_see_all_shelters_spec.rb
@@ -10,5 +10,55 @@ RSpec.describe "shelters' index page", type: :feature do
       expect(page).to have_content(shelter_1.name)
       expect(page).to have_content(shelter_2.name)
     end
+    it "can see a link to edit a shelter's information" do
+      shelter_1 = Shelter.create(name: "Dumb Friends League", address: "3", city: "Denver", state: "CO", zip: "81029")
+      shelter_2 = Shelter.create(name: "Denver Animal Shelter", address: "3", city: "Denver", state: "CO", zip: "81029")
+
+      visit "/shelters"
+      click_link("Update Shelter", match: :first)
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+
+      name = "Denver Pet Rescue"
+
+      fill_in :name, with: name
+
+      click_button "Submit"
+      expect(current_path).to eq("/shelters")
+      expect(page).to have_content("Denver Pet Rescue")
+    end
+    it "can see a link to delete a shelter" do
+      shelter_1 = Shelter.create(name: "Dumb Friends League",
+                                 address: "3",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "81029")
+      shelter_2 = Shelter.create(name: "Denver Animal Shelter",
+                                 address: "4",
+                                 city: "Aurora",
+                                 state: "CA",
+                                 zip: "91763")
+
+      visit "/shelters"
+
+      expect(page).to have_content(shelter_1.name)
+      expect(page).to have_link "Delete Shelter", href: "/shelters/#{shelter_1.id}"
+      click_link("Delete Shelter", match: :first)
+
+      expect(current_path).to eq("/shelters")
+      expect(page).to_not have_content(shelter_1.name)
+      expect(current_path).to eq("/shelters")
+      expect(page).to have_content(shelter_2.name)
+    end
+    it "can see a link on each page that takes them to shelter index" do
+      shelter_1 = Shelter.create(name: "Dumb Friends League",
+                                 address: "3",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: "81029")
+
+      all("page").each do
+        |page| page[href="/shelters"]
+      end
+    end
   end
 end
