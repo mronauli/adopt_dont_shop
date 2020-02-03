fe312fb06c9da3b4cd5ce7509536455b6c5509dc
Maria Ronauli <mronauli@gmail.com>
2020-02-02 20:54:14 -0700

    add tests for shelter and pet models

---
+++ spec/models/pet_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Pet, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :picture }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :sex }
+    it { should validate_presence_of :age }
+    it { should validate_presence_of :description}
+  end
+  describe 'relationships' do
+    it {should belong_to :shelter}
+  end
+end
