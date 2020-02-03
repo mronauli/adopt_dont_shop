7f5914b7eca0506eb56cc1473fb4e2136eaa02e0
Maria Ronauli <mronauli@gmail.com>
2020-02-02 18:11:43 -0700

    Add new and create actions to shelters controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,4 +1,18 @@
 class SheltersController < ApplicationController
   def index
+    @shelters = Shelter.all
   end
+
+  def new
+  end
+
+  def create
+    shelter = Shelter.create(shelter_params)
+    redirect_to "/shelters"
+  end
+
+  private
+    def shelter_params
+      params.permit(:name, :address, :city, :state, :zip)
+    end
 end
