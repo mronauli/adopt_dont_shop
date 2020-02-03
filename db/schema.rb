2da0c57bbcfdc21fe0bf085021b3347a95648aa0
Maria Ronauli <mronauli@gmail.com>
2020-02-02 21:43:15 -0700

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200203031931) do
+ActiveRecord::Schema.define(version: 20200203044041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20200203031931) do
     t.string "name"
     t.string "description"
     t.string "age"
-    t.string "adoption_status"
     t.string "sex"
+    t.string "adoption_status"
     t.bigint "shelter_id"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
