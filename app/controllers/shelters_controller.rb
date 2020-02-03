7949cb8d5583cd661a459ead36152583e63e6033
Maria Ronauli <mronauli@gmail.com>
2020-02-02 18:52:35 -0700

    add destroy action to shelters controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -26,6 +26,12 @@ class SheltersController < ApplicationController
     redirect_to "/shelters"
   end
 
+  def destroy
+    @shelter = Shelter.find params[:id]
+    @shelter.destroy
+    redirect_to "/shelters"
+  end
+
   private
     def shelter_params
       params.permit(:name, :address, :city, :state, :zip)
