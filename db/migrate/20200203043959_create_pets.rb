2e87110bd226c743996de0c89f20143be789644a
Maria Ronauli <mronauli@gmail.com>
2020-02-02 21:42:06 -0700

    add migrations

---
+++ db/migrate/20200203043959_create_pets.rb
@@ -0,0 +1,12 @@
+class CreatePets < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pets do |t|
+      t.string :picture
+      t.string :name
+      t.string :description
+      t.string :age
+      t.string :sex
+      t.string :adoption_status
+    end
+  end
+end
